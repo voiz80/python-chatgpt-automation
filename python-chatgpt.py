8b38ca9f108e3667ef09e6ce7eb9a50025d364cc
Ivo Dimitrov <idimitrov@easypay.bg>
2023-03-15 14:39:09 +0200

    update

--- python-chatgpt.py
+++ python-chatgpt.py
@@ -1,11 +1,6 @@
 import requests
-import argparse
 import os
-
-parser = argparse.ArgumentParser()
-parser.add_argument("prompt", help="The prompt to send to the OpenAI API")
-parser.add_argument("file_name", help="Name of the file to save Python script")
-args = parser.parse_args()
+import menu
 
 api_endpoint = "https://api.openai.com/v1/completions"
 api_key = os.getenv("OPENAI_API_KEY")
@@ -17,16 +12,16 @@ request_headers = {
 
 req_data = {
     "model": "text-davinci-003",
-    "prompt": f"Write python script to {args.prompt}. Provide only code, no text",
+    "prompt": f"Hello Chat GPT3. {menu.final_prompt}",
     "max_tokens": 500,
     "temperature": 0.5
 }
-
+print(f"Your choice is: {menu.final_prompt} Name of the file: {menu.file_name}")
 response = requests.post(api_endpoint, headers=request_headers, json=req_data)
 
 if response.status_code == 200:
     response_text = response.json()["choices"][0]["text"]
-    with open(args.file_name, "w") as file:
+    with open(menu.file_name, "w") as file:
         file.write(response_text)
 else:
     print(f"Request failed with status code: {str(response.status_code)}")
